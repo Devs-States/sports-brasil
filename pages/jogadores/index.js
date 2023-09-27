311c546021507b09e28f477e4ebad1c98c5f6596
Alex Santos <alexsantos.web.developer@gmail.com>
2023-09-27 17:09:18 -0300

    card effect

--- pages/jogadores/index.js
+++ pages/jogadores/index.js
@@ -39,6 +39,35 @@ function carrossel() {
 setInterval(carrossel, 1800);
 
 
+// zoom in cards 
+
+const elementoPai = document.querySelector('.carrosselBox');
+const intervaloDeTempo = 1800; // Tempo em milissegundos (2 segundos, por exemplo)
+let indiceAtual = 1;
+
+function aplicarClasse() {
+  // Remova a classe de todos os elementos filhos
+  for (let i = 1; i <= 24; i++) {
+    elementoPai.children[i].classList.remove('cardGrow');
+  }
+
+  // Adicione a classe ao elemento atual
+  elementoPai.children[indiceAtual].classList.add('cardGrow');
+
+  // Avance para o próximo elemento
+  indiceAtual++;
+
+  // Se chegarmos ao índice 25, volte ao índice 1
+  if (indiceAtual > 24) {
+    indiceAtual = 1;
+  }
+}
+
+// Chame a função inicialmente
+aplicarClasse();
+
+// Configurar um intervalo de tempo para chamar a função repetidamente
+setInterval(aplicarClasse, intervaloDeTempo);
 
 // Card ==> Popup
 
@@ -46,16 +75,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const cards = document.querySelectorAll(".card")
 
   cards.forEach((card) => {
+    
     card.addEventListener("click", () => {
+      card.classList.add('cardGrow');
       const cardClone = card.cloneNode(true);
       const popup = document.createElement("div");
-      popup.classList.add("popup");
+      popup.classList.add("popup"); 
       popup.appendChild(cardClone);
       document.body.appendChild(popup);
-
+      
       // Centralizar o popup
       popup.classList.add("popup-opened");
-
+      
       // Fechar o popup ao clicar fora dele
       popup.addEventListener("click", (event) => {
         if (event.target === popup) {
