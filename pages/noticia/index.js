310d848da30f53ec9a8afebd7aee077ecdf8829d
GabrielBerg4mini <gabrielbergaminioficial@gmail.com>
2023-10-18 19:16:48 -0300

    primeiro conteudo noticia

--- pages/noticia/index.js
+++ pages/noticia/index.js
@@ -1,3 +1,4 @@
+//header
 const buttonMenuMobile = document.getElementById("buttonMenu")
 
 function toggleMenu(event) {
@@ -15,3 +16,32 @@ function toggleMenu(event) {
 
 buttonMenuMobile.addEventListener("click", toggleMenu)
 buttonMenuMobile.addEventListener("touchstart", toggleMenu)
+//header
+
+// timer noticia
+function calcularTempoDecorrido() {
+  const tempoDecorridoElemnt = document.getElementById("timer-noticia")
+  const dataPostagem = new Date("2023-10-18T18:23:00")
+
+  setInterval(() => {
+    const agora = new Date()
+    const diferenca = agora - dataPostagem
+    const segundosDecorridos = Math.floor(diferenca / 1000)
+    const minutosDecorridos = Math.floor(segundosDecorridos / 60)
+    const horasDecorridas = Math.floor(minutosDecorridos / 60)
+    const diasDecorridos = Math.floor(horasDecorridas / 24)
+
+    const tempoDecorrido =
+      diasDecorridos > 0
+        ? `${diasDecorridos} dia(s) atrás`
+        : horasDecorridas > 0
+        ? `${horasDecorridas} hora(s) atrás`
+        : minutosDecorridos > 0
+        ? `${minutosDecorridos} minuto(s) atrás`
+        : `${segundosDecorridos} segundo(s) atrás`
+
+    tempoDecorridoElemnt.textContent = tempoDecorrido
+  }, 1000)
+}
+calcularTempoDecorrido()
+// timer noticia
